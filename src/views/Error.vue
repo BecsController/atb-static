b333061faf8443bbe4339f0f4e4e137e423d6ddc
becs <becs@storypark.com>
2020-09-21 11:14:46 +1200

    Fixing 500 page and adding error assets

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="Error">
+  <div class="error-page mt-n10" cover>
     <vue-title title=" | Error"></vue-title>
-    <v-container>
-      <v-img :src="errorPage" contain />
-    </v-container>
+    <v-spacer class="mb-10"> </v-spacer>
+    <v-col cols="10" offset="1">
+      <v-row align="center" class="mt-10 pa-10 error-box">
+        <v-col cols="5" offset="1" align-self="start">
+          <v-img class="message" :src="message" />
+          <v-col cols="8" offset="4">
+            <v-btn
+              color="orange"
+              class="white--text"
+              @click="$router.push('/')"
+            >
+              {{ $t('error.button') }}
+            </v-btn>
+          </v-col>
+        </v-col>
+        <v-col cols="5" offset="1" align-self="end">
+          <v-img class="mt-16 fen" :src="fen" />
+        </v-col>
+      </v-row>
+    </v-col>
   </div>
 </template>
 
 <script>
-import errorPage from '@/assets/500.png';
+import message from '@/assets/500_message.png';
+import fen from '@/assets/404_fen.png';
 
 export default {
   data: () => ({
-    errorPage
+    message,
+    fen
   })
 };
 </script>
