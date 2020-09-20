5790622255af0d37b5bf44df7ffae3681d31749c
becs <becs@storypark.com>
2020-09-21 11:12:53 +1200

    Redo error page with css so it's functional and looks relatively the same and uses up all page real estate

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="NotFound">
+  <div class="error-page mt-n10" cover>
     <vue-title title=" | Not Found"></vue-title>
-    <v-container>
-      <v-img :src="notFound" contain />
-    </v-container>
+    <v-spacer class="mb-10"> </v-spacer>
+    <v-col cols="10" offset="1">
+      <v-row align="center" class="mt-10 pa-10 error-box">
+        <v-col cols="5" offset="1" align-self="start">
+          <v-img class="message" :src="message" />
+          <v-col cols="8" offset="4">
+            <v-btn
+              color="orange"
+              class="white--text"
+              @click="$router.push('/')"
+            >
+              {{ $t('error.button') }}
+            </v-btn>
+          </v-col>
+        </v-col>
+        <v-col cols="5" offset="1" align-self="end">
+          <v-img class="mt-16 fen" :src="fen" />
+        </v-col>
+      </v-row>
+    </v-col>
   </div>
 </template>
 
 <script>
-import notFound from '@/assets/404.png';
+import message from '@/assets/404_message.png';
+import fen from '@/assets/404_fen.png';
 
 export default {
   data: () => ({
-    notFound
+    message,
+    fen
   })
 };
 </script>
