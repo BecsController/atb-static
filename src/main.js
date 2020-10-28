a6a13535a608496fd18efebad72a5e41ea51554e
becs <becs@storypark.com>
2020-10-28 14:55:52 +1300

    Revert "Trying to add cache"
    This reverts commit 447253dc5a929a101aa127252cf72319423df58e.

--- src/main.js
+++ src/main.js
@@ -7,14 +7,6 @@ import vueHeadful from 'vue-headful';
 
 Vue.config.productionTip = false;
 
-// Check that service workers are supported
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
-  });
-}
-
 new Vue({
   router,
   vuetify,
--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "globDirectory": "src/",
-  "globPatterns": [
-    "**/*.{vue,png,js,json,scss}"
-  ],
-  "swDest": "src/sw.js"
-};
