79fcc48e50436f14e7ec8c832f3db9e8d285d0b5
becs <becs@storypark.com>
2020-10-28 15:23:26 +1300

    Give me back my favicon

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pwa: {
+    appleMobileWebAppCapable: 'yes',
+    iconPaths: {
+      favicon32: 'img/icons/favicon.ico',
+      favicon16: 'img/icons/favicon.ico',
+      appleTouchIcon: 'img/icons/favicon.ico',
+      maskIcon: 'img/icons/favicon.ico',
+      msTileImage: 'img/icons/favicon.ico'
+    }
+  }
+}
