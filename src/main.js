8bd3bc9138f7e02b15fb7bcfecb0c5e4fdf6e582
becs <becs@storypark.com>
2020-11-04 15:25:06 +1300

    Trying lazy image plugin

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
+import { VLazyImagePlugin } from 'v-lazy-image';
 import i18n from './i18n';
 import vueHeadful from 'vue-headful';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+Vue.use(VLazyImagePlugin);
 
 new Vue({
   router,
